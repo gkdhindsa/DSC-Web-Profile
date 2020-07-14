f0b5fd07311cd44b4b3b53b2d39324e516cfadbd
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-07-15 00:05:42 +0530

    dsc wbe profile

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+$(document).on('ready', function () {
+  $(".regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+});
+
+function updateTab(arg){
+  console.log(arg.id);
+  document.querySelector(".content-section").innerHTML=document.querySelector('.'+arg.id).innerHTML;
+}
+
+function updatePhoto(arg){
+  document.querySelector('#slide-main').src=arg.src;
+}
+var slides=document.querySelectorAll("#slide");
+
+slides.forEach(slide => {
+  slide.addEventListener('click', function(){
+    updatePhoto(slide)
+  });
+    
+});
+
+var tabs=document.querySelectorAll(".tab-icon");
+tabs.forEach(tab => {
+    tab.addEventListener('click', function(){
+      updateTab(tab)
+    });
+});
