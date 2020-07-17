bda96c2e449fdf09bfaf8a72e67a5a9eb53f0ca4
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-07-17 22:10:15 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-$(document).on('ready', function () {
-  document.querySelector(".content-section").innerHTML=document.querySelector('.about').innerHTML;
-
-
-});
-
-
 function updateTab(arg){
   document.querySelector(".content-section").innerHTML=document.querySelector('.'+arg.id).innerHTML;
 }
@@ -14,12 +7,18 @@ function updatePhoto(arg){
   document.querySelector('#slide-main').src=arg.src;
 }
 var slides=document.querySelectorAll("#slide");
+console.log(slides);
+slides.forEach(slide => 
+  slide.addEventListener('click', 
+  function(){
+    console.log('hello');
+  })  
+);
+
 slides.forEach(slide => {
-  slide.addEventListener('click', function(){
-    console.log(slide);
-    updatePhoto(slide)
-  });
-    
+    slide.addEventListener('click', function(){
+      console.log(slide);
+    })
 });
 
 var tabs=document.querySelectorAll(".tab-icon");
