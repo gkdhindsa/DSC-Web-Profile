78e858b3d7fa04dc81985e033cfbaf8441724f5d
Gurnehmat Kaur Dhindsa <gurnehmat.dhindsa@gmail.com>
2020-07-18 18:41:59 +0530

    responsive slider added- updatePhoto function not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,37 @@ function updatePhoto(arg){
   console.log(arg);
   document.querySelector('#slide-main').src=arg.src;
 }
-var slides=document.querySelectorAll("#slide");
-console.log(slides);
-slides.forEach(slide => 
-  slide.addEventListener('click', 
-  function(){
-    console.log('hello');
-  })  
-);
-
-slides.forEach(slide => {
-    slide.addEventListener('click', function(){
-      console.log(slide);
-    })
-});
 
 var tabs=document.querySelectorAll(".tab-icon");
 tabs.forEach(tab => {
     tab.addEventListener('click', function(){
       updateTab(tab)
+    });
+});
+
+$('.carousel[data-type="multi"] .item').each(function() {
+	var next = $(this).next();
+	if (!next.length) {
+		next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+
+	for (var i = 0; i < 2; i++) {
+		next = next.next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+
+		next.children(':first-child').clone().appendTo($(this));
+	}
+});
+
+
+
+var slides=document.querySelectorAll("#slide");
+slides.forEach(slide => {
+    slide.addEventListener('click', function(){
+      console.log(slide);
+      updatePhoto(slide);
     });
 });
